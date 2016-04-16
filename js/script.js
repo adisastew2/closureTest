228fc5c052d0c001ba487b0f6b4212e90081563e
Adisa Stewart <adisa.stewart1988@gmail.com>
2016-04-15 20:23:46 -0700

    onload funtion

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-function makeCounter() { 
-	var count = 0;
-	function counter() {
-		count = count + 1; return count;
-	}
-	return counter; 
-}
-var doCount = makeCounter(); 
-
-console.log(doCount()); 
-console.log(doCount()); 
-console.log(doCount());
+window.onload = function() {
+	
+};
